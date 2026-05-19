804e40b5b64246d32a877724dc6f0a6a9b66ae99
Melania Tampubolon <melania.tampubolon@gmail.com>
2026-05-19 22:50:58 +0700

    Added basic form handling and Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function generateMood(event) {
+  event.preventDefault();
 
+  let userInput = document.querySelector("#user-input");
+
+  new Typewriter("#result", {
+    strings: `🌴 ${userInput.value} Escape<br />
+    Golden sunsets, ocean breeze,<br />
+    slow mornings and peaceful nights.`,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let travelFormElement = document.querySelector("#travel-form");
+
+travelFormElement.addEventListener("submit", generateMood);
