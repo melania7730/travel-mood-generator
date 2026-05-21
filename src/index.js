4ff2b34a8eca7ff3952642b83cb663faeaeb43cc
Melania Tampubolon <melania.tampubolon@gmail.com>
2026-05-21 23:35:21 +0700

    Updated for better user experience

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateMood(event) {
   let apiKey = "b5dca18t5009430fdddbc6b9afdb4d2o";
 
   let context =
-    "You are a creative travel mood expert and love to write short dreamy travel moods. Your mission is to generate a 4 line travel mood in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title. Do not include markdown or ```html. Sign the travel mood with 'Travel Mood AI' inside a <strong> element at the end.";
+    "You are a creative travel mood expert and love to write short dreamy travel moods. Your mission is to generate a 4 line travel mood in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Do not include a title. Do not include markdown or ```html.Sign the travel mood with <br /><br /><strong>Travel Mood AI</strong> at the end ";
 
   let prompt = `Generate a travel mood for ${userInput.value}`;
 
@@ -24,7 +24,11 @@ function generateMood(event) {
 
   let resultElement = document.querySelector("#result");
 
-  resultElement.innerHTML = `✈️ Generating travel mood for ${userInput.value}...`;
+  resultElement.classList.remove("hidden");
+
+  resultElement.innerHTML = `<div class="generating">
+  ✈️ Generating travel mood for ${userInput.value}...
+  </div>`;
 
   axios.get(apiUrl).then(displayMood);
 }
